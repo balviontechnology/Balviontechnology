661e0bbdb0a3c49e07d951f3dd8ca67e03d5a502
Surender662001 <subramaniansiva727@gmail.com>
2025-12-28 22:15:05 +0530

    PORT added

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,6 @@ app.use(cors({
 
 app.use("/api/contact", contactRoute);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
