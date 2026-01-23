71ed805d4ed2a03a4aa40ee3f16524fc8bff8e25
balviontechnology <hr.balvion@gmail.com>
2026-01-23 22:31:51 +0530

    New  add on backend v10

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -9,7 +9,7 @@ export default function ThankYou() {
       <div className="min-h-screen flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-4">Thank You!</h1>
         <p className="text-lg text-gray-600">
-          We’ve received your message and will get back to you soon.
+          We’ve received your message and will get back to you soon...
         </p>
       </div>
 
